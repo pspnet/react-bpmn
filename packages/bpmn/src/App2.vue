b5a35d1639b79678ce037373298438ed4d974103
pspnet <104241378+pspnet@users.noreply.github.com>
2023-02-23 14:58:36 +0000

    test

---
+++ packages/bpmn/src/App2.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {ref} from "vue"
+  import hljs from 'highlight.js/lib/core';
+import xml from 'highlight.js/lib/languages/xml';
+hljs.registerLanguage('xml', xml);
+
+const value=ref<string>("pppoop");
+</script>
+<template>
+<div contenteditable="true"><pre><code>{{ value }}</code>
+</pre></div>
+</template>
